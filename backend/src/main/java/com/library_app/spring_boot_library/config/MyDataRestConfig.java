d301e278409b739b7d54fce91dfd08d1d08b3111
Luka Baturić <luka.baturic@gmail.com>
2025-08-14 20:31:09 +0200

    Setup backend repository for review data

--- backend/src/main/java/com/library_app/spring_boot_library/config/MyDataRestConfig.java
+++ backend/src/main/java/com/library_app/spring_boot_library/config/MyDataRestConfig.java
@@ -6,6 +6,7 @@
 package com.library_app.spring_boot_library.config;
 
 import com.library_app.spring_boot_library.entity.Book;
+import com.library_app.spring_boot_library.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -27,8 +28,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         };
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /* CORS */
         cors.addMapping(config.getBasePath() + "/**")
---
+++ backend/src/main/java/com/library_app/spring_boot_library/entity/Review.java
@@ -0,0 +1,38 @@
+/**
+ * @author Luka Baturić
+ * @date 12/08/2025
+ */
+
+package com.library_app.spring_boot_library.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "review")
+@Data
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "userEmail")
+    private String userEmail;
+
+    @Column(name = "date")
+    @CreationTimestamp
+    private Date date;
+
+    @Column(name = "rating")
+    private double rating;
+
+    @Column(name = "book_id")
+    private Long bookId;
+
+    @Column(name = "review_description")
+    private String reviewDescription;
+}
