bb13f642db7f217385b7951a924b09159b86c4c1
Luka Baturić <luka.baturic@gmail.com>
2025-08-18 21:22:56 +0200

    Configure additional application security on the backend

---
+++ backend/src/main/java/com/library_app/spring_boot_library/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+/**
+ * @author Luka Baturić
+ * @date 18/08/2025
+ */
+
+package com.library_app.spring_boot_library.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers("/api/books/secure/**")
+                                .authenticated().anyRequest().permitAll())
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()))
+                .cors(withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.setSharedObject(ContentNegotiationStrategy.class,
+                new HeaderContentNegotiationStrategy());
+
+        Okta.configureResourceServer401ResponseBody(http);
+
+        return http.build();
+    }
+}
